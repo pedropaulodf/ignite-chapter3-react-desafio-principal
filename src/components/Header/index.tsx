5afa5691343ff6f71bad6ff045d7b723805d8f83
Pedro Paulo <pedropaulotj@gmail.com>
2022-02-14 00:26:08 -0300

    fix: minor adjusts

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,7 +10,7 @@ export default function Header() {
         <div className={styles.headerContent}>
           <Link href="/" >
             <a>
-              <img src="/logo.svg" alt="logo" />
+              <img src="/Logo.svg" alt="Logo" />
             </a>
           </Link>
           
